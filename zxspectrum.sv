d9daed6f004767cc3a4edcfa79a3a78f2e4e8c71
Gyorgy Szombathelyi <gyurco@freemail.hu>
2018-04-29 21:32:52 +0200

    Add bit5 for paging in Pentagon mode
    7ffd bit5 is used for the highest paging bit in Pentagon1024
    effd bit2 restores its original functionality (disable paging)
    
    This makes it a Pentagon1024. Now Robocop1024 mix works:
    https://vtrd.in/remix/ROBO1024.ZIP

--- zxspectrum.sv
+++ zxspectrum.sv
@@ -132,13 +132,13 @@ localparam CONF_STR1 = {
 	"O23,Stereo mix,none,25%,50%,100%;",
 	"-;",
 	"ODE,Features,ULA+ & Timex,ULA+,Timex,None;",
-	"OAC,Memory,Spectrum 128K/+2,Pentagon 512K,Profi 1024K,Spectrum 48K,Spectrum +2A/+3;"
+	"OAC,Memory,Spectrum 128K/+2,Pentagon 1024K,Profi 1024K,Spectrum 48K,Spectrum +2A/+3;"
 };
 
 localparam CONF_STR2 = {
 	"2,Reset & apply;",
 	"J,Fire 1,Fire 2;",
-	"V,v3.77.",`BUILD_DATE
+	"V,v3.80.",`BUILD_DATE
 };
 
 
@@ -477,19 +477,21 @@ dpram #(.ADDRWIDTH(18), .NUMWORDS(180224), .MEM_INIT_FILE("bios.mif")) rom
 );
 
 reg        zx48;
-reg        p512;
+reg        p1024;
 reg        pf1024;
 reg        plus3;
 reg        page_scr_copy;
 reg        shadow_rom;
 reg  [7:0] page_reg;
 reg  [7:0] page_reg_plus3;
-wire       page_disable = zx48 | page_reg[5];
+reg  [7:0] page_reg_p1024;
+wire       page_disable = zx48 | (~p1024 & page_reg[5]) | (p1024 & page_reg_p1024[2] & page_reg[5]);
 wire       page_scr     = page_reg[3];
 wire [5:0] page_ram     = {page_128k, page_reg[2:0]};
-wire       page_write   = ~addr[15] & ~addr[1] & (addr[14] | ~plus3) & ~page_disable;
-wire       page_write_plus3 = ~addr[1] & addr[12] & ~addr[13] & ~addr[14] & ~addr[15] & plus3 & ~page_disable;
+wire       page_write   = ~addr[15] & ~addr[1] & (addr[14] | ~plus3) & ~page_disable; //7ffd
+wire       page_write_plus3 = ~addr[1] & addr[12] & ~addr[13] & ~addr[14] & ~addr[15] & plus3 & ~page_disable; //1ffd
 wire       page_special = page_reg_plus3[0];
+wire       page_p1024 = addr[15] & addr[14] & addr[13] & ~addr[12] & ~addr[3]; //eff7
 reg  [2:0] page_128k;
 
 reg  [3:0] page_rom;
@@ -520,16 +522,17 @@ always @(posedge clk_sys) begin
 		page_scr_copy <= 0;
 		page_reg    <= 0;
 		page_reg_plus3 <= 0; 
+		page_reg_p1024 <= 0;
 		page_128k   <= 0;
 		page_reg[4] <= Fn[10];
 		page_reg_plus3[2] <= Fn[10];
 		shadow_rom <= shdw_reset & ~plusd_en;
 		if(Fn[10] && (rmod == 1)) begin
-			p512   <= 0;
+			p1024  <= 0;
 			pf1024 <= 0;
 			zx48   <= ~plus3;
 		end else begin
-			p512  <= (status[12:10] == 1);
+			p1024 <= (status[12:10] == 1);
 			pf1024<= (status[12:10] == 2);
 			zx48  <= (status[12:10] == 3);
 			plus3 <= (status[12:10] == 4);
@@ -541,12 +544,13 @@ always @(posedge clk_sys) begin
 		if(io_wr & ~old_wr) begin
 			if(page_write) begin
 				page_reg  <= cpu_dout;
-				if(p512)  page_128k[1:0] <= cpu_dout[7:6];
+				if(p1024 & ~page_reg_p1024[2])	page_128k[2:0] <= { cpu_dout[5], cpu_dout[7:6] };
 				if(~plusd_mem) page_scr_copy <= page_reg[3];
 			end else if (page_write_plus3) begin
 				page_reg_plus3 <= cpu_dout; 
 			end
 			if(pf1024 & (addr == 'hDFFD)) page_128k <= cpu_dout[2:0];
+			if(p1024 & page_p1024) page_reg_p1024 <= cpu_dout;
 		end
 	end
 end
