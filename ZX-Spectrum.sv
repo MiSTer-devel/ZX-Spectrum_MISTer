bf406f123647b7c47fd91290de31a9743c712c17
sorgelig <pour.garbage@gmail.com>
2021-02-26 21:52:34 +0800

    Update sys. HV-Integer scale.

--- ZX-Spectrum.sv
+++ ZX-Spectrum.sv
@@ -38,8 +38,9 @@ module emu
 	output        CE_PIXEL,
 
 	//Video aspect ratio for HDMI. Most retro systems have ratio 4:3.
-	output [11:0] VIDEO_ARX,
-	output [11:0] VIDEO_ARY,
+	//if VIDEO_ARX[12] or VIDEO_ARY[12] is set then [11:0] contains scaled size instead of aspect ratio.
+	output [12:0] VIDEO_ARX,
+	output [12:0] VIDEO_ARY,
 
 	output  [7:0] VGA_R,
 	output  [7:0] VGA_G,
@@ -196,6 +197,13 @@ localparam CONF_BDI   = "(BDI)";
 localparam CONF_PLUSD = "(+D) ";
 localparam CONF_PLUS3 = "(+3) ";
 
+// Status Bit Map:
+//              Upper                          Lower
+// 0         1         2         3          4         5         6
+// 01234567890123456789012345678901 23456789012345678901234567890123
+// 0123456789ABCDEFGHIJKLMNOPQRSTUV 0123456789ABCDEFGHIJKLMNOPQRSTUV
+// XXXXXXX XXXXXXXXXXXXXXXXXXXX
+
 `include "build_id.v"
 localparam CONF_STR = {
 	"Spectrum;;",
@@ -211,6 +219,7 @@ localparam CONF_STR = {
 	"P1OFG,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%;",
 	"P1-;",
 	"d1P1O1,Vertical Crop,No,Yes;",
+	"P1OQR,Scale,Normal,V-Integer,Narrower HV-Integer,Wider HV-Integer;",
 	"P1-;",
 	"P1OKL,General Sound,512KB,1MB,2MB,Disabled;",
 	"P1O23,Stereo Mix,none,25%,50%,100%;",
@@ -902,7 +911,7 @@ always @(posedge CLK_VIDEO) begin
 	ce_pix <= ce1;
 end
 
-reg [3:0] Rx, Gx, Bx;
+reg [7:0] Rx, Gx, Bx;
 reg       hs,vs,hbl,vbl;
 always @(posedge CLK_VIDEO) if (ce_pix) begin
 	hs <= HSync;
@@ -912,9 +921,9 @@ always @(posedge CLK_VIDEO) if (ce_pix) begin
 	vbl <= VBlank;
 
 	casex({ulap_ena, ulap_mono})
-		'b0X: {Gx,Rx,Bx} <= {G, {3{I & G}}, R, {3{I & R}}, B, {3{I & B}}};
-		'b10: {Gx,Rx,Bx} <= {ulap_color[7:5], ulap_color[7], ulap_color[4:2], ulap_color[4], ulap_color[1:0], ulap_color[1:0]};
-		'b11: {Gx,Rx,Bx} <= {ulap_color, 4'b0000};
+		'b0X: {Gx,Rx,Bx} <= {G, {3{I & G}}, G, {3{I & G}}, R, {3{I & R}}, R, {3{I & R}}, B, {3{I & B}}, B, {3{I & B}}};
+		'b10: {Gx,Rx,Bx} <= {{2{ulap_color[7:5]}}, ulap_color[7:6], {2{ulap_color[4:2]}}, ulap_color[4:3], {4{ulap_color[1:0]}}};
+		'b11: {Gx,Rx,Bx} <= {3{ulap_color}};
 	endcase
 end
 
@@ -923,7 +932,7 @@ reg wide;
 always @(posedge CLK_VIDEO) begin
 	vcrop <= 0;
 	wide <= 0;
-	if(HDMI_WIDTH >= (HDMI_HEIGHT + HDMI_HEIGHT[11:1])) begin
+	if(HDMI_WIDTH >= (HDMI_HEIGHT + HDMI_HEIGHT[11:1]) && !forced_scandoubler && !scale) begin
 		if(HDMI_HEIGHT == 480)  vcrop <= 240;
 		if(HDMI_HEIGHT == 600)  begin vcrop <= 200; wide <= vcrop_en; end
 		if(HDMI_HEIGHT == 720)  vcrop <= 240;
@@ -937,26 +946,21 @@ end
 wire [1:0] ar = status[5:4];
 wire vcrop_en = status[1];
 wire vga_de;
-video_crop video_crop
+video_freak video_freak
 (
 	.*,
 	.VGA_DE_IN(vga_de),
 	.ARX((!ar) ? (wide ? 12'd2903 : 12'd3307) : (ar - 1'd1)),
 	.ARY((!ar) ? 12'd2588 : 12'd0),
 	.CROP_SIZE(vcrop_en ? vcrop : 10'd0),
-	.CROP_OFF(0)
+	.CROP_OFF(0),
+	.SCALE(status[27:26])
 );
 
-video_mixer #(.LINE_LENGTH(896), .HALF_DEPTH(1), .GAMMA(1)) video_mixer
+video_mixer #(.LINE_LENGTH(896), .GAMMA(1)) video_mixer
 (
 	.*,
-
-	.clk_vid(CLK_VIDEO),
-	.ce_pix(ce_pix),
-	.ce_pix_out(CE_PIXEL),
-
 	.hq2x(scale == 1),
-	.scanlines(0),
 	.scandoubler(scale || forced_scandoubler),
 
 	.VGA_DE(vga_de),
@@ -966,8 +970,7 @@ video_mixer #(.LINE_LENGTH(896), .HALF_DEPTH(1), .GAMMA(1)) video_mixer
 	.VBlank(vbl),
 	.R(Rx),
 	.G(Gx),
-	.B(Bx),
-	.mono(ulap_ena & ulap_mono)
+	.B(Bx)
 );
 
 assign VGA_SL = {scale==3, scale==2};
